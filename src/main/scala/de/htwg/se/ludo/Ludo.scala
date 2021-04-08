f8f5c6de83e7da0288cf79fe477d57a7a1560738
lu391see <lukas.seehuber@gmail.com>
2021-04-08 22:23:53 +0200

    rewored ludo to display the game with one println for setup and one for the current game state

--- src/main/scala/de/htwg/se/ludo/Ludo.scala
+++ src/main/scala/de/htwg/se/ludo/Ludo.scala
@@ -5,23 +5,27 @@ import model._
 
 object Ludo {
   def main(args: Array[String]): Unit = {
-    val playerarraysize = scala.io.StdIn.readLine("How many players want to play?\n")
-    var player_counter: Int = 1
+    val playerarraysize = scala.io.StdIn.readLine(
+      """
+        |Welcome to Ludo aka 'Mensch Aergere Dich Nicht'!
+        |How many players want to play?
+        |Type between 1-4: """.stripMargin)
+
     val players:Array[Player] = new Array[Player](playerarraysize.toInt)
-    var string1: String = "Hello"
+    var setup_str: String = "Hello"
+
+    var player_counter: Int = 1
     for (i <- 0 until  playerarraysize.toInt) {
-      players(player_counter - 1) = new Player(scala.io.StdIn.readLine("Type your Name: "), player_counter)
-      string1 += ", "
-      string1 += players(player_counter-1).name
+      val player_name: String = scala.io.StdIn.readLine(s"Player $player_counter, type your name: ")
+      players(player_counter - 1) = Player(player_name, player_counter)
+      setup_str += ", " + players(player_counter - 1).name
       player_counter += 1
     }
-    string1 += "\nWelcome to Mensch Aergere Dich Nicht!\n"
     player_counter -= 1
 
     var game = new Field[Cell](40, Cell(0))
     val tui = new Tui
-    //println(s.stripMargin)
-    println(string1)
+    println(setup_str)
 
     var turn_counter = 0
 
@@ -30,16 +34,18 @@ object Ludo {
 
     while(input != "q") {
       val dice = Dice()
-      println("Current Game Status:" + game.toString + "\n"
-        + players(turn_counter).name + " can walk " + dice.t1 + " please choose a pin (1-4)!")
+      println(s"""
+            |Current Game Status:
+            |${game.toString}
+            |==> ${players(turn_counter).name} can walk ${dice.t1} please choose a pin (1-4)!""".stripMargin)
 
       input = scala.io.StdIn.readLine()
       game = tui.processInputLine(input, game, players(turn_counter), dice)
 
-      if(players(turn_counter).hasWon) {
-        println(players(turn_counter).name + " has won!!")
-        input = "q"//spiel beenden
-      }
+//      if(players(turn_counter).hasWon) {
+//        println(players(turn_counter).name + " has won!!")
+//        input = "q" //spiel beenden
+//      }
 
       turn_counter += 1
       if(turn_counter == player_counter) {
