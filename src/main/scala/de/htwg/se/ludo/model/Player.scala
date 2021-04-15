af981d404fe3b09910ab40c1b14f31d7b423d85e
lu391see <lukas.seehuber@gmail.com>
2021-04-15 11:54:21 +0200

    added style convention

--- src/main/scala/de/htwg/se/ludo/model/Player.scala
+++ src/main/scala/de/htwg/se/ludo/model/Player.scala
@@ -5,18 +5,18 @@ case class Player(name: String, team: Int) {
   //def this(name: String) = this(name, team, defaultPosition, false)
   val defaultPosition : Int = (team-1) * 10
 
-  var hasWon :Boolean = false
+  var hasWon: Boolean = false
 
-  val pin1 = Pin(team * 10 + 1)
+  val pin1: Pin = Pin(team * 10 + 1)
   pin1.addPosition(defaultPosition)
 
-  val pin2 = Pin(team * 10 + 2)
+  val pin2: Pin = Pin(team * 10 + 2)
   pin2.addPosition(defaultPosition)
 
-  val pin3 = Pin(team * 10 + 3)
+  val pin3: Pin = Pin(team * 10 + 3)
   pin3.addPosition(defaultPosition)
 
-  val pin4 = Pin(team * 10 + 4)
+  val pin4: Pin = Pin(team * 10 + 4)
   pin4.addPosition(defaultPosition)
 
   override def toString: String = {
