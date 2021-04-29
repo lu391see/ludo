72f2503660f5cf2204b7bcd3c8e98d5a4aefd519
lu391see <lukas.seehuber@gmail.com>
2021-04-29 11:23:21 +0200

    Game.scala using field.size instead of magic number

--- src/main/scala/de/htwg/se/ludo/model/Game.scala
+++ src/main/scala/de/htwg/se/ludo/model/Game.scala
@@ -3,7 +3,7 @@ package de.htwg.se.ludo.model
 case class Game(field: Field[Cell], players: Vector[Player]) {
 
   def draw_pin(player: Player, pin: Int, dice_roll: Int): Game = {
-    if(player.playerPins(pin).position + dice_roll < player.defaultPosition + 40) {
+    if(player.playerPins(pin).position + dice_roll < player.defaultPosition + field.size) {
       val removed_game = Game(field.replaceCell(player.playerPins(pin).position, Cell(0)), players)
       player.playerPins(pin).addPosition(dice_roll)
       Game(removed_game.field.replaceCell(player.playerPins(pin).position, Cell(player.playerPins(pin).index)), players)
