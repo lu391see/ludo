7ad1b8210a41b69478278bcd4718070cd9018205
lu391see <lukas.seehuber@gmail.com>
2021-04-30 09:49:05 +0200

    reworked tuispec

--- src/test/scala/de/htwg/se/ludo/view/TuiSpec.scala
+++ src/test/scala/de/htwg/se/ludo/view/TuiSpec.scala
@@ -1,37 +1,39 @@
 package de.htwg.se.ludo.view
 
 import de.htwg.se.ludo.model._
+import de.htwg.se.ludo.controller._
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class TuiSpec extends AnyWordSpec with Matchers {
   "A TUI processing pins with a dice of 4" should {
-    val players: Array[Player] = new Array[Player](1)
-    players(0) = Player("", 1)
-    var game = Game(new Field(40, Cell(0)), players.toVector)
-    val tui = new Tui
+
+    val players: Vector[Player] = Vector[Player](Player("", 1))
+    val controller = new Controller(Game(new Field(40, Cell(0)), players))
+    val tui = new Tui(controller)
+
     val first_player = players(0)
     val diceFour = new Dice(4)
 
     "should for the first drawn pin have the fourth cell set to the pin's index" in {
       val input = "1"
-      game = tui.processInputLine(input, game, first_player, diceFour)
-      game.field.spots(4) should be(Cell(first_player.playerPins(0).index))
+      tui.processInputLine(input, first_player, diceFour)
+      controller.game.field.spots(4) should be(Cell(first_player.playerPins(0).index))
     }
     "should for the second drawn pin have the fourth cell set to the pin's index" in {
       val input = "2"
-      game = tui.processInputLine(input, game, first_player, diceFour)
-      game.field.spots(4) should be(Cell(first_player.playerPins(1).index))
+      tui.processInputLine(input, first_player, diceFour)
+      controller.game.field.spots(4) should be(Cell(first_player.playerPins(1).index))
     }
     "should for the third drawn pin have the fourth cell set to the pin's index" in {
       val input = "3"
-      game = tui.processInputLine(input, game, first_player, diceFour)
-      game.field.spots(4) should be(Cell(first_player.playerPins(2).index))
+      tui.processInputLine(input, first_player, diceFour)
+      controller.game.field.spots(4) should be(Cell(first_player.playerPins(2).index))
     }
     "should for the fourth drawn pin have the fourth cell set to the pin's index" in {
       val input = "4"
-      game = tui.processInputLine(input, game, first_player, diceFour)
-      game.field.spots(4) should be(Cell(first_player.playerPins(3).index))
+      tui.processInputLine(input, first_player, diceFour)
+      controller.game.field.spots(4) should be(Cell(first_player.playerPins(3).index))
     }
   }
 }
