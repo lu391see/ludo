7cd61f3e4f830c09c8b72728ce7243ee0a8a7f61
Alexander Brese <admin@alexanderbrese.com>
2021-05-20 16:31:34 +0200

    feat: try and option

--- src/main/scala/de/htwg/se/ludo/controller/DrawState.scala
+++ src/main/scala/de/htwg/se/ludo/controller/DrawState.scala
@@ -2,21 +2,31 @@ package de.htwg.se.ludo.controller
 
 import de.htwg.se.ludo.util.State
 
+import scala.util.Try
+
 case class DrawState(controller: Controller) extends State[GameState] {
   override def handle(input: String, n: GameState): Unit = {
     /*if (input == "n") {
       n.nextState(SetupState(controller))
       return
     }*/
-    val pin = input.toInt // FIXME: Option/Try implementation at this point?
-    if(pin < 1 || pin > 4) {
-      println("please choose a valid pin")
-      return
+    toInt(input) match {
+      case Some(pin) => {
+        if(pin < 1 || pin > 4) {
+          println("please choose a valid pin")
+          return
+        }
+        controller.draw(pin-1)
+      }
+      case None =>
     }
 
-    controller.draw(pin-1)
     controller.game.nextPlayer()
     println(controller.game.currentPlayer + " is next. Press any key to throw a Dice")
     n.nextState(RollState(controller))
+  }
+
+  def toInt(input: String): Option[Int] = {
+    Try(input.toInt).toOption
   }
 }
