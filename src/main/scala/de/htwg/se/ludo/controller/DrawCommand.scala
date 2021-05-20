dd961585006d7747132d2626852647a9a711d0ba
lu391see <lukas.seehuber@gmail.com>
2021-05-20 23:13:27 +0200

    added pips to DrawCommand

--- src/main/scala/de/htwg/se/ludo/controller/DrawCommand.scala
+++ src/main/scala/de/htwg/se/ludo/controller/DrawCommand.scala
@@ -4,23 +4,25 @@ import de.htwg.se.ludo.model.{Game, Player}
 import de.htwg.se.ludo.util.Command
 
 class DrawCommand(pin: Int, controller: Controller) extends Command {
-  var memento: (Game, Player, GameState) = (
+  var memento: (Game, Player, GameState, Int) = (
     controller.game,
     controller.currentPlayer,
-    controller.gameState
+    controller.gameState,
+    controller.pips
   )
 
   override def doStep: Unit = {
-    memento = (controller.game, controller.currentPlayer, controller.gameState)
+    memento = (controller.game, controller.currentPlayer, controller.gameState, controller.pips)
     controller.game = controller.game.draw(controller.currentPlayer, pin, controller.pips)
     controller.notifyObservers()
   }
 
   override def undoStep: Unit = {
-    val new_memento = (controller.game, controller.currentPlayer, controller.gameState)
+    val new_memento = (controller.game, controller.currentPlayer, controller.gameState, controller.pips)
     controller.game = memento._1
     controller.currentPlayer = memento._2
     controller.gameState = memento._3
+    controller.pips = memento._4
     memento = new_memento
     controller.notifyObservers()
   }
