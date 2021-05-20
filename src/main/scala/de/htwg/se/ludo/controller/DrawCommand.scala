19570d00e1063c0c2d6ec077c0f055711430e52d
lu391see <lukas.seehuber@gmail.com>
2021-05-20 22:22:21 +0200

    drawCommand resets GameState but doesnt reset internal pin position

--- src/main/scala/de/htwg/se/ludo/controller/DrawCommand.scala
+++ src/main/scala/de/htwg/se/ludo/controller/DrawCommand.scala
@@ -4,27 +4,26 @@ import de.htwg.se.ludo.model.{Game, Player}
 import de.htwg.se.ludo.util.Command
 
 class DrawCommand(pin: Int, controller: Controller) extends Command {
-  var memento: (Game, Player) = (controller.game, controller.currentPlayer)
+  var memento: (Game, Player, GameState) = (
+    controller.game,
+    controller.currentPlayer,
+    controller.gameState
+  )
 
   override def doStep: Unit = {
-    memento = (controller.game, controller.currentPlayer)
+    memento = (controller.game, controller.currentPlayer, controller.gameState)
     controller.game = controller.game.draw(controller.currentPlayer, pin, controller.pips)
     controller.notifyObservers()
   }
 
   override def undoStep: Unit = {
-    val new_memento = (controller.game, controller.currentPlayer)
+    val new_memento = (controller.game, controller.currentPlayer, controller.gameState)
     controller.game = memento._1
     controller.currentPlayer = memento._2
+    controller.gameState = memento._3
     memento = new_memento
     controller.notifyObservers()
   }
 
-  override def redoStep: Unit = {
-    val new_memento = (controller.game, controller.currentPlayer)
-    controller.game = memento._1
-    controller.currentPlayer = memento._2
-    memento = new_memento
-    controller.notifyObservers()
-  }
+  override def redoStep: Unit = undoStep
 }
