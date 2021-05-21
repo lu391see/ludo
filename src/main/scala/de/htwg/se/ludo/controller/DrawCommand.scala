6e48657b7429a09274e70f8bc3daa49887c9da2d
lu391see <lukas.seehuber@gmail.com>
2021-05-21 08:36:03 +0200

    added pins to DrawCommand, working as expected

--- src/main/scala/de/htwg/se/ludo/controller/DrawCommand.scala
+++ src/main/scala/de/htwg/se/ludo/controller/DrawCommand.scala
@@ -1,14 +1,16 @@
 package de.htwg.se.ludo.controller
 
-import de.htwg.se.ludo.model.{Game, Player}
+import de.htwg.se.ludo.model.{Game, Pin, Player}
 import de.htwg.se.ludo.util.Command
 
 class DrawCommand(pin: Int, controller: Controller) extends Command {
-  var memento: (Game, Player, GameState, Int) = (
+
+  var memento: (Game, Player, GameState, Int, Vector[Pin]) = (
     controller.game,
     controller.currentPlayer,
     controller.gameState,
-    controller.pips
+    controller.pips,
+    getPins
   )
 
   override def doStep: Unit = {
@@ -18,14 +20,30 @@ class DrawCommand(pin: Int, controller: Controller) extends Command {
   }
 
   override def undoStep: Unit = {
-    val new_memento = (controller.game, controller.currentPlayer, controller.gameState, controller.pips)
+    val new_memento = (controller.game, controller.currentPlayer, controller.gameState, controller.pips, getPins)
+
     controller.game = memento._1
     controller.currentPlayer = memento._2
     controller.gameState = memento._3
     controller.pips = memento._4
+    controller.players.foreach { player =>
+      if (player == controller.currentPlayer) {
+        player.team.pins = memento._5
+      }
+    }
     memento = new_memento
     controller.notifyObservers()
   }
 
   override def redoStep: Unit = undoStep
+
+  private def getPins: Vector[Pin] = {
+    var pins: Vector[Pin] = Vector.empty
+    controller.players.foreach { player =>
+      if (player == controller.currentPlayer) {
+        pins = player.team.pins
+      }
+    }
+    pins
+  }
 }
