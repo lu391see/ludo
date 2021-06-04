521d561bdbaf58b07a43f230ca9d181e258992ac
Alexander Brese <admin@alexanderbrese.com>
2021-06-04 08:07:51 +0200

    feat: style actions

--- src/main/scala/de/htwg/se/ludo/aview/gui/Actions.scala
+++ src/main/scala/de/htwg/se/ludo/aview/gui/Actions.scala
@@ -2,6 +2,8 @@ package de.htwg.se.ludo.aview.gui
 
 import de.htwg.se.ludo.controller.Controller
 
+import java.awt.Color
+import javax.swing.border.LineBorder
 import scala.swing.{Button, Component, Dimension, FlowPanel}
 
 case class Actions (controller: Controller) extends FlowPanel {
@@ -15,6 +17,15 @@ case class Actions (controller: Controller) extends FlowPanel {
   val redoButton: Button = Button("Redo") {
     controller.redo()
   }
+
+  rollButton.background = Color.red.brighter().darker()
+  rollButton.border = new LineBorder(Color.white, 1)
+
+  undoButton.background = Color.green.brighter().darker()
+  undoButton.border = new LineBorder(Color.white, 1)
+
+  redoButton.background = Color.yellow.brighter().darker()
+  redoButton.border = new LineBorder(Color.white, 1)
 
   rollButton.preferredSize_=(new Dimension(120, 30))
   undoButton.preferredSize_=(new Dimension(120, 30))
