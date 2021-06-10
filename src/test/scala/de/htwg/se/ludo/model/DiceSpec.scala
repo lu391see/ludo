abf878b78c0f19d8163b4c665126497df6e9ceb8
lu391see <lukas.seehuber@gmail.com>
2021-06-10 12:51:49 +0200

    added test for 8sided dice

--- src/test/scala/de/htwg/se/ludo/model/DiceSpec.scala
+++ src/test/scala/de/htwg/se/ludo/model/DiceSpec.scala
@@ -1,17 +1,28 @@
 package de.htwg.se.ludo.model
-import de.htwg.se.ludo.model.diceComponent.dice6Impl.Dice
+import de.htwg.se.ludo.model.diceComponent._
 
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers
 
 class DiceSpec extends AnyWordSpec with Matchers{
-  "A Dice " when {
+  "A 6 sided Dice " when {
     "thrown" should {
-      val dice = Dice()
+      val dice = dice6Impl.Dice()
       "have a value between 1 and 6" in {
         val diceValue = dice.pips
         diceValue.isInstanceOf[Int]
         diceValue.toString matches "1|2|3|4|5|6"
+      }
+    }
+  }
+
+  "A 8 sided Dice " when {
+    "thrown" should {
+      val dice = dice8Impl.Dice()
+      "have a value between 1 and 6" in {
+        val diceValue = dice.pips
+        diceValue.isInstanceOf[Int]
+        diceValue.toString matches "1|2|3|4|5|6|7|8"
       }
     }
   }
