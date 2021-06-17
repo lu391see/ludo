8707b896ee0b765c0efc244abf2bc863179cf280
lu391see <lukas.seehuber@gmail.com>
2021-06-17 14:12:28 +0200

    cleanup

--- src/main/scala/de/htwg/se/ludo/LudoModule.scala
+++ src/main/scala/de/htwg/se/ludo/LudoModule.scala
@@ -5,8 +5,7 @@ import com.google.inject.name.Names
 import de.htwg.se.ludo.controller.controllerComponent.ControllerInterface
 import de.htwg.se.ludo.controller.controllerComponent.controllerBaseImpl.Controller
 import de.htwg.se.ludo.model.{AllPinWinStrategy, OnePinWinStrategy}
-import de.htwg.se.ludo.model.diceComponent.{DiceInterface, dice6Impl, dice8Impl}
-import de.htwg.se.ludo.model.gameComponent.{BoardInterface, CellInterface, GameInterface}
+import de.htwg.se.ludo.model.gameComponent.CellInterface
 import de.htwg.se.ludo.model.gameComponent.gameBaseImpl._
 import de.htwg.se.ludo.util.WinStrategy
 import net.codingwell.scalaguice.ScalaModule
@@ -22,22 +21,12 @@ class LudoModule extends AbstractModule with ScalaModule {
   override def configure(): Unit = {
     bind[ControllerInterface].to[Controller]
 
-    // GameComponent
-    // bind[GameInterface].to[Game]
-    //bind[BoardInterface].to[Board]
-    //bind[CellInterface].to[Cell]
-
-    //bind[BoardInterface].annotatedWithName("NewGame").toInstance(new Board(defaultSize, Cell(""), totalPins))
-
-    // DiceComponent
-    //bind[DiceInterface].annotatedWithName("D6").toInstance(new dice6Impl.Dice)
-    //bind[DiceInterface].annotatedWithName("D8").toInstance(new dice8Impl.Dice)
-
     bind[WinStrategy].annotatedWithName("OnePin").toInstance(new OnePinWinStrategy)
     bind[WinStrategy].annotatedWithName("AllPin").toInstance(new AllPinWinStrategy)
 
-    bindConstant().annotatedWith(Names.named("DefaultSize")).to(defaultSize)
     bind[CellInterface].annotatedWith(Names.named("EmptyCell")).toInstance(Cell(""))
+
+    bindConstant().annotatedWith(Names.named("DefaultSize")).to(defaultSize)
     bindConstant().annotatedWith(Names.named("TotalPins")).to(totalPins)
     bindConstant().annotatedWith(Names.named("MaxPlayers")).to(maxPlayers)
 
