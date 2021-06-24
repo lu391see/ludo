53b082dbfa07b02feb8d7adde59f949d859cfa58
lu391see <lukas.seehuber@gmail.com>
2021-06-24 21:55:43 +0200

    pin full coverage

--- src/test/scala/de/htwg/se/ludo/model/playerComponent/PinSpec.scala
+++ src/test/scala/de/htwg/se/ludo/model/playerComponent/PinSpec.scala
@@ -13,6 +13,21 @@ class PinSpec extends AnyWordSpec with Matchers{
       "change position after add" in {
         pinY1.move(10).position should be(10)
       }
+      "be based at base position + index" in {
+        pinY1.base(0).position should be(0)
+      }
+    }
+    "set to Y2, 1" should {
+      val pinY2 = Pin("Y2", 1)
+      "have position" in {
+        pinY2.position should be(1)
+      }
+      "change position after add" in {
+        pinY2.move(10).position should be(10)
+      }
+      "be based at base position + index" in {
+        pinY2.base(0).position should be(1)
+      }
     }
   }
 }
