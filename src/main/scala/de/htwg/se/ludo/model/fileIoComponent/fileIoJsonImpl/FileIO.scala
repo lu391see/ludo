bec9563ea69359bb75b329469689c71084eb80da
Alexander Brese <admin@alexanderbrese.com>
2021-06-25 12:42:25 +0200

    make it work.

--- src/main/scala/de/htwg/se/ludo/model/fileIoComponent/fileIoJsonImpl/FileIO.scala
+++ src/main/scala/de/htwg/se/ludo/model/fileIoComponent/fileIoJsonImpl/FileIO.scala
@@ -1,12 +1,13 @@
 package de.htwg.se.ludo.model.fileIoComponent.fileIoJsonImpl
 
 import de.htwg.se.ludo.model.fileIoComponent.FileIOInterface
-import de.htwg.se.ludo.model.gameComponent.GameInterface
+import de.htwg.se.ludo.model.gameComponent.gameBaseImpl.{Board, Cell, Game}
+import de.htwg.se.ludo.model.gameComponent.{BoardInterface, GameInterface}
+import de.htwg.se.ludo.model.playerComponent.{Player, Team}
 import de.htwg.se.ludo.model.playerComponent.Player
+import play.api.libs.json.{JsLookupResult, JsNumber, JsObject, JsValue, Json}
 
-import play.api.libs.json.{JsNumber, JsValue, Json, Writes}
-import de.htwg.se.ludo.model.playerComponent.Player
-
+import java.awt.Color
 import scala.io.Source
 
 class FileIO extends FileIOInterface {
@@ -14,43 +15,85 @@ class FileIO extends FileIOInterface {
     val source: String = Source.fromFile("ludo.json").getLines().mkString
     val json: JsValue = Json.parse(source)
     val currentPlayerIndex = (json \ "currentPlayerIndex").get.toString().toInt
-    val player_amount = (json \"amount").get.toString().toInt
-//    for (index <- 0 until )
-//    (Game(), currentPlayerIndex)
-    (null, 0)
+    val playerAmount = (json \ "playerAmount").get.toString().toInt
+    val spots = json \\ "value"
+    val players = json \ "players"
+    val baseSize = 16
+
+    (
+      Game(
+        boardFromJson(spots, baseSize),
+        playersFromJson(players, playerAmount)
+      ),
+      currentPlayerIndex
+    )
   }
 
-  def gameToJson(currentPlayer: Player, players: Vector[Player], game: GameInterface) = {
+  def gameToJson(
+      currentPlayer: Player,
+      players: Vector[Player],
+      game: GameInterface
+  ) = {
     Json.obj(
       "currentPlayerIndex" -> JsNumber(players.lastIndexOf(currentPlayer)),
-      "amount" -> JsNumber(players.size),
+      "playerAmount" -> JsNumber(players.size),
       "players" -> Json.toJson(
         for (player <- game.players)
           yield Json.obj(
             "name" -> player.name,
             "color" -> player.team.toColorString.toString,
-            "basePosition"-> JsNumber(player.team.basePosition),
+            "basePosition" -> JsNumber(player.team.basePosition),
             "startPosition" -> JsNumber(player.team.startPosition),
-            "homePosition" -> JsNumber(player.team.homePosition),
+            "homePosition" -> JsNumber(player.team.homePosition)
           )
       ),
       "board" -> Json.toJson(
-        for (index <- 0 until game.board.spots.size)
+        for (index <- game.board.spots.indices)
           yield {
-            var obj = Json.obj(
-              "id" -> JsNumber(index),
+            Json.obj(
+              "value" -> game.board.spots(index).value
             )
-            if (game.board.spots(index).isSet) {
-              obj = obj.++(Json.obj(
-                "value" -> game.board.spots(index).value
-              ))
-            }
-            obj
           }
       )
     )
   }
 
+  def boardFromJson(
+      spots: scala.collection.Seq[JsValue],
+      baseSize: Int
+  ): BoardInterface = {
+    Board(spots.map(spot => Cell(spot.as[String])).toVector, baseSize)
+  }
+
+  def playersFromJson(
+      players: JsLookupResult,
+      playerAmount: Int
+  ): Vector[Player] = {
+
+    (0 until playerAmount)
+      .map(i =>
+        Player(
+          (players(i) \ "name").as[String],
+          new Team(
+            fromColorString((players(i) \ "color").as[String].charAt(0)),
+            (players(i) \ "basePosition").as[Int],
+            (players(i) \ "startPosition").as[Int],
+            (players(i) \ "homePosition").as[Int]
+          )
+        )
+      )
+      .toVector
+  }
+
+  def fromColorString(color: Char): Color = {
+    color match {
+      case 'B' => Color.black
+      case 'R' => Color.red
+      case 'Y' => Color.yellow
+      case 'G' => Color.green
+      case _   => Color.white
+    }
+  }
 
   override def save(currentPlayer: Player, game: GameInterface): Unit = {
     import java.io._
