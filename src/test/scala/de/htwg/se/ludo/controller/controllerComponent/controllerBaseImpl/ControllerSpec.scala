67d4139f011a4193a209a02c5907ac9c901ea9e2
lu391see <lukas.seehuber@gmail.com>
2021-06-27 16:14:50 +0200

    controllerspec refactor v1

--- src/test/scala/de/htwg/se/ludo/controller/controllerComponent/controllerBaseImpl/ControllerSpec.scala
+++ src/test/scala/de/htwg/se/ludo/controller/controllerComponent/controllerBaseImpl/ControllerSpec.scala
@@ -1,27 +1,55 @@
 package de.htwg.se.ludo.controller.controllerComponent.controllerBaseImpl
 
-import de.htwg.se.ludo.model.playerComponent.{Player, Team}
+import de.htwg.se.ludo.controller.controllerComponent.controllerBaseImpl.gameStates.{DrawState, RollState, SetupState}
+import de.htwg.se.ludo.util.{EnterPlayerNameMessage, NoCurrentPlayerMessage}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-import java.awt.Color
-
 class ControllerSpec extends AnyWordSpec with Matchers {
   "A Controller" when {
-    "observed by an Observer" should {
-      val players: Vector[Player] = Vector[Player](Player("p2", new Team(Color.yellow, 0, 15, 55)))
-      //val game = new Game(Board(40, new Cell(" ")), players)
+    "being a Publisher" should {
       val controller = new Controller()
-      "notify its Observer after setting a cell" in {
-        //controller.draw(players(0),0,4)
-        //observer.updated should be(true)
-        //controller.game.field.cell(4).value should be (11)
+      "should start with the SetupState" in {
+        controller.gameState.state should be (SetupState(controller))
       }
-      "notify its Observer after creation" in {
-        //controller.createStartGame(4, players)
-        //observer.updated should be(true)
-        //controller.game.field.size should be(4)
+      "where it adds players via the SetupState" in {
+        val empty_input = ""
+        controller.handleInput(empty_input)
+        controller.message should be (EnterPlayerNameMessage)
+
+        controller.handleInput("lukas")
+        controller.handleInput("alex")
+        controller.players.size should be (2)
       }
+      "and enters RollState once 4 players are added or 'start' is the input" in {
+        controller.handleInput("hans")
+        controller.handleInput("peter")
+        controller.handleInput("start")
+        controller.gameState.state should be (RollState(controller))
+      }
+      "show error messages if current player is missing while switching" in {
+        controller.currentPlayer = None
+        controller.switchPlayer()
+        controller.message should be (NoCurrentPlayerMessage)
+      }
+      "show error message if current player is missing while rolling a Dice" in {
+        controller.currentPlayer = None
+        controller.rollDice()
+        controller.message should be (NoCurrentPlayerMessage)
+      }
+      "and return the message as string (to be used from the GUI textfield)" in {
+        controller.messageToString() should be (NoCurrentPlayerMessage.toString)
+      }
+      "after rolling a dice get into DrawState" in {
+        /* fixme: next state is not predictable. Set one pin of the current player on the field first
+        controller.currentPlayer = Some(controller.players(0))
+        controller.gameState.state = RollState(controller)
+        controller.handleInput("...")
+        controller.gameState.state should be (DrawState(controller))*/
+      }
+      "beating an enemy pin when ahead" in {
+      }
+
 
     }
   }
